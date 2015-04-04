55642eaacf8b737c2fb5fe73e01d8b3f88ef037d
XTLi <xtli1689@163.com>
2015-04-04 10:48:05 -0400

    Finish task8 to task9

--- ExpediaTest/CarTest.cs
+++ ExpediaTest/CarTest.cs
@@ -85,5 +85,23 @@ namespace ExpediaTest
             mocks.VerifyAll();
 
         }
+
+        //task 9 demonstrate object mother
+        [TestMethod]
+        public void TestThatCarDoesGetCorrectCarLocationUsingObjectMother()
+        {
+            IDatabase mockDB = mocks.StrictMock<IDatabase>();
+            string currentLocation = "Chicago";
+
+            Expect.Call(mockDB.getCarLocation(376)).Return(currentLocation);
+
+            mocks.ReplayAll();
+
+            var target = ObjectMother.BMW();
+            target.Database = mockDB;
+            string result = target.getCarLocation(376);
+            Assert.AreEqual(currentLocation, result);
+            mocks.VerifyAll();
+        }
 	}
 }
